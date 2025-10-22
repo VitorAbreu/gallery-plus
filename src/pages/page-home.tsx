32e597d5f6afec93450281a7ba4f5e3bc1fe3ba0
Vitor <vitoryanfma@gmail.com>
2025-10-21 21:24:48 -0300

    feat(lista): criação layout de lista de fotos, com skeleton

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,26 +1,10 @@
 import Container from "../components/container";
-import type { Photo } from "../contexts/photos/models/photo";
-import PhotoWidget from "./../contexts/photos/components/photo-widget";
+import PhotosList from "../contexts/photos/components/photos-list";
 
 export default function PageHome() {
   return (
     <Container>
-      <div className="grid grid-cols-4 gap-9">
-        <PhotoWidget
-          photo={{
-            id: "123",
-            title: "olá mundo",
-            imageId: "portrait-tower.png",
-            albums: [
-              { id: "3421", title: "Album 1" },
-              { id: "3421", title: "Album 2" },
-              { id: "3421", title: "Album 3" },
-            ],
-          }}
-          loading={false}
-        />
-        <PhotoWidget photo={{} as Photo} loading={true} />
-      </div>
+      <PhotosList photos={[]} loading />
     </Container>
   );
 }

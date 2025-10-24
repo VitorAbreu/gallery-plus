8cb7b9716eadb831bcefc690e3b4a03ca7b25c58
Vitor <vitoryanfma@gmail.com>
2025-10-24 19:19:13 -0300

    feat(photo details): criação da páginação de fotos, mais image preview e botão excluir

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,13 +1,57 @@
-import { useParams } from "react-router";
+import Container from "../components/container";
 import Text from "../components/text";
+import type { Photo } from "../contexts/photos/models/photo";
+import Skeleton from "../components/skeleton";
+import PhotosNavigator from "../contexts/photos/components/photos-navigator";
+import ImagePreview from "../components/image-preview";
+import Button from "../components/button";
 
 export default function PagePhotoDetails() {
-  const { id } = useParams();
+  // const { id } = useParams();
+  // apenas testes
+  const isLoadingPhoto = true;
+  const photo = {
+    id: "123",
+    title: "Teste",
+    imageId: "portrait-tower.png",
+    albums: [
+      { id: "4321", title: "Album 1" },
+      { id: "431", title: "Album 2" },
+      { id: "421", title: "Album 3" },
+    ],
+  } as Photo;
+
   return (
-    <>
-      <Text variant="heading-medium">Página Detalhe da Foto</Text>
-      <hr />
-      <Text>Id da foto é {id}</Text>
-    </>
+    <Container>
+      <header className="flex items-center justify-between gap-8 mb-8">
+        {!isLoadingPhoto ? (
+          <Text variant="heading-large">{photo?.title}</Text>
+        ) : (
+          <Skeleton className="w-48 h-8" />
+        )}
+
+        <PhotosNavigator loading={isLoadingPhoto} />
+      </header>
+
+      <div className="grid grid-cols-[21rem] gap-24">
+        <div className="space-y-3">
+          {!isLoadingPhoto ? (
+            <ImagePreview
+              src={`/images/${photo?.imageId}`}
+              title={photo.title}
+              imageClassName="h-[21rem]"
+            />
+          ) : (
+            <Skeleton className="h-[21rem]" />
+          )}
+
+          {!isLoadingPhoto ? (
+            <Button variant="destructive">Excluir</Button>
+          ) : (
+            <Skeleton className="w-20 h-10" />
+          )}
+        </div>
+      </div>
+    </Container>
   );
 }

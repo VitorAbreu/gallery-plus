245ef11a79888b3f1c05a205c7a3ebf5606a46e4
Vitor <vitoryanfma@gmail.com>
2025-11-12 10:35:31 -0300

    feat(toaster): adicionando feedback de toaster ao criar uma nova foto

--- src/contexts/photos/hooks/use-photo.ts
+++ src/contexts/photos/hooks/use-photo.ts
@@ -2,6 +2,7 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { api, fetcher } from "../../../helpers/api";
 import type { Photo } from "../models/photo";
 import type { PhotoNewFormSchema } from "../schemas";
+import { toast } from "sonner";
 
 interface PhotoDetailResponse extends Photo {
   nextPhotoId?: string;
@@ -18,7 +19,6 @@ export default function usePhoto(id?: string) {
   const queryClient = useQueryClient();
 
   async function createPhoto(payload: PhotoNewFormSchema) {
-    // eslint-disable-next-line no-useless-catch
     try {
       // renaming data to photo, destructuring and using alias
       const { data: photo } = await api.post<Photo>("/photos", {
@@ -45,7 +45,10 @@ export default function usePhoto(id?: string) {
 
       // invalida a lista de fotos em cache para que a foto atual apareça na lista
       queryClient.invalidateQueries({ queryKey: ["photos"] });
+
+      toast.success("Foto criada com sucesso");
     } catch (error) {
+      toast.error("Erro ao criar foto");
       throw error;
     }
   }
